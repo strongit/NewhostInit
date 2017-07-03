41e9ffc79815f4e31fd33317df8c9713367f6675
root <root@cacti69.(none)>
2017-07-03 10:41:17 +0800

    go lang

---
+++ golang/userage.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+type UserAges struct {
+	ages map[string]int
+	sync.Mutex
+}
+
+func (ua *UserAges) Add(name string, age int) {
+	ua.Lock()
+	defer ua.Unlock()
+	ua.ages[name] = age
+}
+
+func (ua *UserAges) Get(name string) int {
+	if age, ok := ua.ages[name]; ok {
+		return age
+	}
+	return -1
+}
